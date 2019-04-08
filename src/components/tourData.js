dfcfbb66ba9892cbbf6b50e2ec1153416a1ec74b
Mahran Elmasri <mahranelmasri@Mahrans-MBP.local>
2019-04-08 15:40:23 +0200

    first commit

---
+++ src/components/tourData.js
@@ -0,0 +1,98 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpeg",
+    name: "new york bridge tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "paris lights tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpeg",
+    name: "london royal palace tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 5,
+    city: "Roma",
+    img: "./img/roma.jpeg",
+    name: "Roma tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 6,
+    city: "Rio De Jenaro",
+    img: "./img/rio de janeiro.jpeg",
+    name: "Rio De Jenaro tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 7,
+    city: "Canads",
+    img: "./img/canada.jpeg",
+    name: "Monterial wald tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 8,
+    city: "Prag",
+    img: "./img/prag.jpeg",
+    name: "Prag city tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 9,
+    city: "Patra",
+    img: "./img/petra.jpeg",
+    name: "Petra old city tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 10,
+    city: "china",
+    img: "./img/china.jpeg",
+    name: "Chi mau city tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 11,
+    city: "ukrania",
+    img: "./img/ukrania.jpeg",
+    name: "ukrania royal palace tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 12,
+    city: "istanbul",
+    img: "./img/istanbul.jpeg",
+    name: "istanbul birdge tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  }
+];
